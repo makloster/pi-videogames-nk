c9cc39ab451f82173cf867c7c4882f038ad1047a
Nekrocow <nekrocow@gmail.com>
2022-06-02 22:44:36 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 const routes = require('./routes/index.js')
+const { CORS_URL } = process.env
 
 require('./db.js')
 
@@ -14,7 +15,7 @@ server.use(express.json({ limit: '50mb' }))
 server.use(cookieParser())
 server.use(morgan('dev'))
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', CORS_URL) // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
